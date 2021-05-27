5a5cb9d72604627057290c53ba315e5aacdca1ef
cola314 <ubj020314@gamil.com>
2021-05-27 23:02:51 +0900

    -update api
    key "compactVideoRenderer" has been changed to "videoRenderer"

--- src/lib/parser.service.ts
+++ src/lib/parser.service.ts
@@ -1,36 +1,36 @@
 export class ParserService {
 
   parseVideo(data: any) {
-    if (!data || !data.compactVideoRenderer) return;
+    if (!data || !data.videoRender) return;
 
-    let title = data.compactVideoRenderer.title.runs[0].text;
+    let title = data.videoRender.title.runs[0].text;
     title = title.replace("\\\\", "\\");
     title = decodeURIComponent(title);
 
     const res = {
       id: {
-        videoId: data.compactVideoRenderer.videoId
+        videoId: data.videoRender.videoId
       },
-      url: `https://www.youtube.com/watch?v=${data.compactVideoRenderer.videoId}`,
+      url: `https://www.youtube.com/watch?v=${data.videoRender.videoId}`,
       title,
-      description: data.compactVideoRenderer.descriptionSnippet && data.compactVideoRenderer.descriptionSnippet.runs[0] ? data.compactVideoRenderer.descriptionSnippet.runs[0].text : "",
-      duration_raw: data.compactVideoRenderer.lengthText ? data.compactVideoRenderer.lengthText.simpleText : null,
+      description: data.videoRender.descriptionSnippet && data.videoRender.descriptionSnippet.runs[0] ? data.videoRender.descriptionSnippet.runs[0].text : "",
+      duration_raw: data.videoRender.lengthText ? data.videoRender.lengthText.simpleText : null,
       snippet: {
-        url: `https://www.youtube.com/watch?v=${data.compactVideoRenderer.videoId}`,
-        duration: data.compactVideoRenderer.lengthText ? data.compactVideoRenderer.lengthText.simpleText : null,
-        publishedAt: data.compactVideoRenderer.publishedTimeText ? data.compactVideoRenderer.publishedTimeText.simpleText : null,
+        url: `https://www.youtube.com/watch?v=${data.videoRender.videoId}`,
+        duration: data.videoRender.lengthText ? data.videoRender.lengthText.simpleText : null,
+        publishedAt: data.videoRender.publishedTimeText ? data.videoRender.publishedTimeText.simpleText : null,
         thumbnails: {
-          id: data.compactVideoRenderer.videoId,
-          url: data.compactVideoRenderer.thumbnail.thumbnails[data.compactVideoRenderer.thumbnail.thumbnails.length - 1].url,
-          default: data.compactVideoRenderer.thumbnail.thumbnails[data.compactVideoRenderer.thumbnail.thumbnails.length - 1],
-          high: data.compactVideoRenderer.thumbnail.thumbnails[data.compactVideoRenderer.thumbnail.thumbnails.length - 1],
-          height: data.compactVideoRenderer.thumbnail.thumbnails[data.compactVideoRenderer.thumbnail.thumbnails.length - 1].height,
-          width: data.compactVideoRenderer.thumbnail.thumbnails[data.compactVideoRenderer.thumbnail.thumbnails.length - 1].width
+          id: data.videoRender.videoId,
+          url: data.videoRender.thumbnail.thumbnails[data.videoRender.thumbnail.thumbnails.length - 1].url,
+          default: data.videoRender.thumbnail.thumbnails[data.videoRender.thumbnail.thumbnails.length - 1],
+          high: data.videoRender.thumbnail.thumbnails[data.videoRender.thumbnail.thumbnails.length - 1],
+          height: data.videoRender.thumbnail.thumbnails[data.videoRender.thumbnail.thumbnails.length - 1].height,
+          width: data.videoRender.thumbnail.thumbnails[data.videoRender.thumbnail.thumbnails.length - 1].width
         },
         title,
-        views: data.compactVideoRenderer.viewCountText && data.compactVideoRenderer.viewCountText.simpleText ? data.compactVideoRenderer.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
+        views: data.videoRender.viewCountText && data.videoRender.viewCountText.simpleText ? data.videoRender.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
       },
-      views: data.compactVideoRenderer.viewCountText && data.compactVideoRenderer.viewCountText.simpleText ? data.compactVideoRenderer.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
+      views: data.videoRender.viewCountText && data.videoRender.viewCountText.simpleText ? data.videoRender.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
     };
 
     return res;
