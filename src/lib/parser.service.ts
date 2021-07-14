f78fe9f045cc1b5716388587ca872218205e54de
Dave Koller <davidkoller7117@gmail.com>
2021-07-14 21:07:11 +0200

    adapt error handling

--- src/lib/parser.service.ts
+++ src/lib/parser.service.ts
@@ -2,37 +2,41 @@ export class ParserService {
 
   parseVideo(data: any) {
     if (!data || !data.videoRenderer) return;
+    let title = ''
+    try{
+      try{
+        title = data.videoRenderer.title.runs[0].text;
+        title = title.replace("\\\\", "\\");
+        title = decodeURIComponent(title);
+      }catch (e) {}
 
-    let title = data.videoRenderer.title.runs[0].text;
-    title = title.replace("\\\\", "\\");
-    title = decodeURIComponent(title);
-
-    const res = {
-      id: {
-        videoId: data.videoRenderer.videoId
-      },
-      url: `https://www.youtube.com/watch?v=${data.videoRenderer.videoId}`,
-      title,
-      description: data.videoRenderer.descriptionSnippet && data.videoRenderer.descriptionSnippet.runs[0] ? data.videoRenderer.descriptionSnippet.runs[0].text : "",
-      duration_raw: data.videoRenderer.lengthText ? data.videoRenderer.lengthText.simpleText : null,
-      snippet: {
-        url: `https://www.youtube.com/watch?v=${data.videoRenderer.videoId}`,
-        duration: data.videoRenderer.lengthText ? data.videoRenderer.lengthText.simpleText : null,
-        publishedAt: data.videoRenderer.publishedTimeText ? data.videoRenderer.publishedTimeText.simpleText : null,
-        thumbnails: {
-          id: data.videoRenderer.videoId,
-          url: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].url,
-          default: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1],
-          high: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1],
-          height: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].height,
-          width: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].width
+      return {
+        id: {
+          videoId: data.videoRenderer.videoId
         },
+        url: `https://www.youtube.com/watch?v=${data.videoRenderer.videoId}`,
         title,
+        description: data.videoRenderer.descriptionSnippet && data.videoRenderer.descriptionSnippet.runs[0] ? data.videoRenderer.descriptionSnippet.runs[0].text : "",
+        duration_raw: data.videoRenderer.lengthText ? data.videoRenderer.lengthText.simpleText : null,
+        snippet: {
+          url: `https://www.youtube.com/watch?v=${data.videoRenderer.videoId}`,
+          duration: data.videoRenderer.lengthText ? data.videoRenderer.lengthText.simpleText : null,
+          publishedAt: data.videoRenderer.publishedTimeText ? data.videoRenderer.publishedTimeText.simpleText : null,
+          thumbnails: {
+            id: data.videoRenderer.videoId,
+            url: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].url,
+            default: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1],
+            high: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1],
+            height: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].height,
+            width: data.videoRenderer.thumbnail.thumbnails[data.videoRenderer.thumbnail.thumbnails.length - 1].width
+          },
+          title,
+          views: data.videoRenderer.viewCountText && data.videoRenderer.viewCountText.simpleText ? data.videoRenderer.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
+        },
         views: data.videoRenderer.viewCountText && data.videoRenderer.viewCountText.simpleText ? data.videoRenderer.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
-      },
-      views: data.videoRenderer.viewCountText && data.videoRenderer.viewCountText.simpleText ? data.videoRenderer.viewCountText.simpleText.replace(/[^0-9]/g, "") : 0
-    };
-
-    return res;
+      };
+    }catch (e){
+      return undefined
+    }
   }
 }
