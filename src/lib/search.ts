7805cf69f491c26d61631b687d96409cafb646c5
Jascha Dachtera <jascha.dachtera@googlemail.com>
2022-03-24 17:59:18 +0100

    Cleanup, add types and remove "got" dependency

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,5 +1,5 @@
-import { ParserService } from './parser.service';
-import got from 'got';
+import { parseVideo } from './parseVideo';
+import { ParsedVideSearchResponse, VideoDetails } from './types';
 
 const rfc3986EncodeURIComponent = (str: string) => encodeURIComponent(str).replace(/[!'()*]/g, escape);
 
@@ -7,58 +7,65 @@ export async function searchVideo(searchQuery: string) {
   const YOUTUBE_URL = 'https://www.youtube.com';
 
   const results = [];
-  let details = [];
-  let fetched = false;
-  const options = { type: "video", limit: 0 };
 
-  const searchRes: any = await got.get(`${YOUTUBE_URL}/results?q=${rfc3986EncodeURIComponent(searchQuery.trim())}&hl=en`);
-  let html = await searchRes.body;
-  // try to parse html
+  const options = { type: 'video', limit: 0 };
+
+  const searchRes = await fetch(`${YOUTUBE_URL}/results?q=${rfc3986EncodeURIComponent(searchQuery.trim())}&hl=en`);
+  let html = await searchRes.text();
+
   try {
     const data = html.split("ytInitialData = '")[1].split("';</script>")[0];
-    // @ts-ignore
-    html = data.replace(/\\x([0-9A-F]{2})/ig, (...items) => {
+    html = data.replace(/\\x([0-9A-F]{2})/gi, (...items) => {
       return String.fromCharCode(parseInt(items[1], 16));
     });
-    html = html.replaceAll("\\\\\"", "");
-    html = JSON.parse(html)
-  } catch(e) { /* nothing */}
-
-  if(html && html.contents && html.contents.sectionListRenderer && html.contents.sectionListRenderer.contents
-    && html.contents.sectionListRenderer.contents.length > 0 && html.contents.sectionListRenderer.contents[0].itemSectionRenderer &&
-    html.contents.sectionListRenderer.contents[0].itemSectionRenderer.contents.length > 0){
-    details = html.contents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
-    fetched = true;
-  }
-  // backup/ alternative parsing
-  if (!fetched) {
-    try {
-      details = JSON.parse(html.split('{"itemSectionRenderer":{"contents":')[html.split('{"itemSectionRenderer":{"contents":').length - 1].split(',"continuations":[{')[0]);
-      fetched = true;
-    } catch (e) { /* nothing */
-    }
-  }
-  if (!fetched) {
-    try {
-      details = JSON.parse(html.split('{"itemSectionRenderer":')[html.split('{"itemSectionRenderer":').length - 1].split('},{"continuationItemRenderer":{')[0]).contents;
-      fetched = true;
-    } catch(e) { /* nothing */ }
+    html = html.replaceAll('\\\\"', '');
+  } catch {
+    /* nothing */
   }
 
-  if (!fetched) return [];
+  const details = parseSearchResponse(html);
 
-  // tslint:disable-next-line:prefer-for-of
-  for (let i = 0; i < details.length; i++) {
-    if (typeof options.limit === "number" && options.limit > 0 && results.length >= options.limit) break;
-    const data = details[i];
+  console.log(details);
 
-    const parserService = new ParserService();
-    const parsed = parserService.parseVideo(data);
-    if (!parsed) continue;
-    const res = parsed;
-
-    results.push(res);
-  }
-
-  return results;
+  return details.slice(0, options.limit > 0 ? options.limit : undefined).map((data) => parseVideo(data));
 }
+
+const parseSearchResponse = (html: string): VideoDetails[] => {
+  // try to parse html
+  try {
+    const parsedHtml: ParsedVideSearchResponse | undefined = JSON.parse(html);
+    const details = parsedHtml?.contents?.sectionListRenderer?.contents?.[0]?.itemSectionRenderer?.contents || [];
+
+    if (details?.length ?? 0 > 0) {
+      return details;
+    }
+  } catch {
+    /* nothing */
+  }
+
+  // backup/ alternative parsing
+
+  try {
+    const details: VideoDetails[] = JSON.parse(
+      html
+        .split('{"itemSectionRenderer":{"contents":')
+        [html.split('{"itemSectionRenderer":{"contents":').length - 1].split(',"continuations":[{')[0],
+    );
+    return details;
+  } catch {
+    /* nothing */
+  }
+
+  try {
+    const details: VideoDetails[] = JSON.parse(
+      html
+        .split('{"itemSectionRenderer":')
+        [html.split('{"itemSectionRenderer":').length - 1].split('},{"continuationItemRenderer":{')[0],
+    ).contents;
+    return details;
+  } catch {
+    /* nothing */
+  }
+
+  return [];
+};
